bfcc789d1fea1304e7f7db8dd5a0bbc5775fc35f
Robert Abraham <robert@adeven.com>
2014-01-03 14:23:45 +0100

    set outfile via commandline

--- bin/failover.pl
+++ bin/failover.pl
@@ -3,8 +3,15 @@
 use strict;
 use warnings;
 use 5.012;
-use lib 'lib';
+
+use Getopt::Long;
 use RedisFailover;
 
-my $failover = RedisFailover->new( out => '/tmp/test.yml' );
+my $out = '/etc/nutcracker/nutcracker.yml';
+
+GetOptions(
+    "out|o=s" => \$out
+);
+
+my $failover = RedisFailover->new( out => $out );
 $failover->run;
