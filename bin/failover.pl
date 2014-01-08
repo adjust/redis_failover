5ef31ed17042d61fb4935301c546a014b63739b6
Robert Abraham <robert@adeven.com>
2014-01-08 12:35:37 +0100

    wait for other failovers to finish

--- bin/failover.pl
+++ bin/failover.pl
@@ -6,12 +6,20 @@ use 5.012;
 
 use Getopt::Long;
 use RedisFailover;
+use File::Basename;
+
+our $PROGNAME = basename($0);
 
 my $out = '/etc/nutcracker/nutcracker.yml';
 
 GetOptions(
     "out|o=s" => \$out
 );
+
+while (1) {
+    my $other_failovers = `pgrep $PROGNAME`;
+    last if $other_failovers eq "";
+}
 
 my $failover = RedisFailover->new( out => $out );
 $failover->run;
